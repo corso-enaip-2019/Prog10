89a459596d048bbd3965ba6b30f7f948d3b0989e
clone1084 <miani.martino@gmail.com>
2019-03-06 17:53:18 +0100

    Nuovo esercizio di gestione classi/studenti

---
+++ Solution1/ListaStudenti/Classe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaStudenti {
+	class Classe {
+		public int NumeroClasse { get; set; }
+		public List<Studente> Studenti { get; set; }
+
+		public Classe(int numero) {
+			NumeroClasse = numero;
+			Studenti = new List<Studente>();
+		}
+		
+		public bool AddStudente(Studente studente) {
+
+			Studenti.Add(studente);
+			studente.ClasseAppartenenza = this;
+
+			return true;
+		}
+
+		public Studente FindStudente(string nomeStudente) {
+			return Studenti.Find(x => x.Nome == nomeStudente);			
+		}
+
+		public bool DeleteStudent(string nomeStudente) {
+			Studente studente = FindStudente(nomeStudente);
+			return Studenti.Remove(studente);
+		}
+
+		internal string PrintStudentList() {
+			string testoStampa = "";
+
+			testoStampa += $"--- Studenti della classe {NumeroClasse}: " + Environment.NewLine;
+			foreach (var studente in Studenti) {
+				testoStampa += studente;
+				testoStampa += Environment.NewLine;
+			}
+			return testoStampa;
+		}
+	}
+}
---
+++ Solution1/ListaStudenti/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaStudenti {
+	class Program {
+
+		const string EXIT = "e";
+		const string STAMPA = "s";
+		const string INSERT = "i";
+		const string DELETE = "d";
+		const string FIND = "t";
+
+
+		static Scuola scuola = new Scuola();
+
+		static void Main(string[] args) {
+
+			bool esci = false;
+
+			while (!esci) {
+				Console.WriteLine($"{EXIT} = esci; {STAMPA} = stampa; {INSERT} = inserisci; {DELETE} = cancella; {FIND} = trova");
+				switch (Console.ReadLine()) {
+					default:
+						//non faccio nulla
+						break;
+					case EXIT:
+						esci = true;
+						break;
+					case STAMPA:
+						PrintClasses();
+						break;
+					case INSERT:
+						AddStudent();
+						break;
+					case DELETE:
+						DeleteStudent();
+						break;
+					case FIND:
+						FindStudent();
+						break;
+				}
+
+				Console.Clear();
+			}
+			
+			Console.ReadLine();
+		}
+
+		private static void PrintClasses() {
+			Console.WriteLine(scuola.PrintClasses());
+			Console.ReadLine();
+		}
+
+		private static string getNomeStudente() {
+			Console.Write("Nome dello studente da trovare: ");
+			return Console.ReadLine();
+		}
+
+		private static List<Classe> findStudent(string nomeStudente) {			
+			List<Classe> classi = new List<Classe>();
+			if (scuola.FindStudent(nomeStudente, out classi)) {
+				return classi;
+			}
+			return null;
+		}
+
+		private static void FindStudent() {
+			List<Classe> classi = findStudent(getNomeStudente());
+			printListClassiStudente(classi);
+			Console.ReadLine();
+		}
+
+		private static void printListClassiStudente(List<Classe> classi) {
+			if (classi.Count == 1) {
+				Console.WriteLine($"Studente trovato in {classi.First().NumeroClasse}");
+			}
+			else {
+				foreach (var c in classi) {
+					Console.WriteLine($"Studente trovato in {c.NumeroClasse}");
+				}
+			}
+		}
+
+		private static void DeleteStudent() {
+			string nomeStudente = getNomeStudente();
+			List<Classe> classi = findStudent(nomeStudente);
+			if (classi.Count == 1) {
+				classi.First().DeleteStudent(nomeStudente);
+			}
+			else {
+				printListClassiStudente(classi);
+				Console.Write("Da quale classe lo vuoi cancellare? ");
+				string classeElimina = Console.ReadLine();
+				///TODO COMPLETARE
+			}
+		}
+
+		static private void AddStudent() {
+			Console.Write("Nome studente: ");
+			string nome = Console.ReadLine();
+			Console.Write("Classe di frequentazione: ");
+			string classe = Console.ReadLine();
+
+			if (scuola.AddStudent(nome, classe)) {
+				Console.Write("Studente aggiunto con successo");
+			}
+			Console.ReadLine();
+		}
+	}
+}
