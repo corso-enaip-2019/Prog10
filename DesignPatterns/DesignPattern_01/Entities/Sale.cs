7ca94ec17556ed89fc8d2c75793c3d48fb7af40a
clone1084 <miani.martino@gmail.com>
2019-04-09 12:55:40 +0200

    Inizio lavori pattern

---
+++ DesignPatterns/DesignPattern_01/Entities/Sale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern_01.Entities
+{
+    class Sale
+    {
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DesignPatterns/DesignPattern_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesignPattern_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * lista impiegati creata come mock
+             * 
+             * per ogni impiegato della lista
+             *      se è il suo giorno di paga
+             *          calcola la sua paga
+             *          crea un record con ID, paga e data di oggi
+             *          salva record in una lista di record
+            */
+
+
+
+        }
+    }
+}
