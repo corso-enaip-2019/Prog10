f36f73236df0f8b2493af47237f10ca4e2d96c93
clone1084 <miani.martino@gmail.com>
2019-04-12 11:00:16 +0200

    Repository, Singleton e inizio di Multiton

---
+++ DesignPatterns/DP_04_Singleton/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DP_04_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mario = new Person { Name = "Mario" };
+            var luigi = new Person { Name = "Luigi" };
+
+            PersonRepository.Instance.Add(mario);
+            PersonRepository.Instance.Add(luigi);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class PersonRepository
+    {
+        private List<Person> _items;
+
+        #region Singleton        
+        /// 
+        /// Implementazione primitiva non thread-safe
+        /// 
+        //static PersonRepository _instance = null;
+        //public static PersonRepository Instance {
+        //    get {
+        //        if (_instance == null)
+        //            _instance = new PersonRepository();
+
+        //        return _instance;
+        //    }
+        //}
+
+        ///Il costruttore statico è thread-safe
+        ///questa è quindi l'implementazione più corretta per la gestione dei singleton
+        static PersonRepository()
+        {
+            Instance = new PersonRepository();
+        }
+        public static PersonRepository Instance { get; }
+
+        private PersonRepository()
+        {
+            _items = new List<Person>();
+        }
+        #endregion Singleton
+
+        public void Add(Person p)
+        {
+            if (p.ID != 0) {
+                throw new ArgumentException("Id già valorizzato!", nameof(Person));
+            }
+
+            var newId = _items.Count == 0
+                ? 1
+                : _items.Max(x => x.ID) + 1;
+
+            p.ID = newId;
+
+            _items.Add(p);
+        }
+
+        public void Update(Person p)
+        {
+            if (p.ID == 0)
+                throw new ArgumentException("Id 0!", nameof(Person));
+            
+            var index = _items.FindIndex(x => x.ID == p.ID);
+            if (index == -1)
+                throw new ArgumentException("Person not registerd!", nameof(Person));
+
+            _items[index] = p;
+        }
+
+        public Person Get(int id)
+        {
+            var p = _items.FirstOrDefault(x => x.ID == id);
+            if (p == null)
+                throw new ArgumentException("Person not registerd!", nameof(Person));
+
+            return p;
+        }
+
+        public IEnumerable<Person> GetAll()
+        {
+            return _items.ToList();
+        }
+
+        public void Remove(int id)
+        {
+            var p = _items.FirstOrDefault(x => x.ID == id);
+            if (p == null)
+                throw new ArgumentException("Person not registerd!", nameof(p));
+
+            _items.Remove(p);
+        }
+    }
+
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DesignPatterns/DP_05_Multiton/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DP_05_Multiton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = CreatePersons();
+            string jsonFile = @"c:\myJson.json";
+            string xmlFile = @"c:\myXML.xml";
+
+            XMLSaver.Instance.Save(persons, xmlFile);
+            JSonSaver.Instance.Save(persons, jsonFile);
+        }
+
+        static List<Person> CreatePersons()
+        {
+            List<Person> persons = new List<Person>();
+
+            persons.Add(new Person() { FullName = "Romolo", Age = 30, Salary = 2000m });
+            persons.Add(new Person() { FullName = "Numa Pompilio", Age = 33, Salary = 1600m });
+            persons.Add(new Person() { FullName = "Tullo Ostilio", Age = 20, Salary = 1200m });
+            persons.Add(new Person() { FullName = "Anco Marzio", Age = 26, Salary = 3000m });
+            persons.Add(new Person() { FullName = "Tarquinio Prisco", Age = 28, Salary = 2200m });
+
+            return persons;
+        }
+    }
+
+    class Person
+    {
+        public string FullName { get; set; }
+        //public DateTime BirthDay { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+    interface ISaver
+    {
+        void Save(List<Person> persons, string fileName);
+    }
+
+    abstract class Saver : ISaver
+    {
+        //public static ISaver GetSaver<T>() where T : Saver
+        //{
+        //    try
+        //    {
+        //        return Activator.CreateInstance<T>();
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw new NotImplementedException("Saver not implemented yet!");
+        //    }
+        //}
+
+        public void Save(List<Person> persons, string fileName)
+        {
+            string message = "";
+            foreach (var p in persons) {
+                message += SerializeObject(p) + Environment.NewLine;
+            }
+
+            System.IO.File.WriteAllText(fileName, message);
+        }
+
+        protected abstract string SerializeObject(Person person);
+    }
+
+    class JSonSaver : Saver
+    {
+        static JSonSaver()
+        {
+            Instance = new JSonSaver();
+        }
+        public static JSonSaver Instance { get; }
+
+        private JSonSaver() { }
+
+        protected override string SerializeObject(Person person)
+        {
+            if (person == null) throw new NullReferenceException($"{nameof(Person)} is null");
+
+            string message = "{";
+
+            message += $"\"{nameof(person.FullName)}\":\"{person.FullName}\",";
+            message += $"\"{nameof(person.Age)}\":{person.Age},";
+            message += $"\"{nameof(person.Salary)}\":{person.Salary}";
+            message += "}";
+
+            return message;
+        }
+    }
+
+    class XMLSaver : Saver
+    {
+        static XMLSaver()
+        {
+            Instance = new XMLSaver();
+        }
+        public static XMLSaver Instance { get; }
+
+        private XMLSaver() { }
+
+        protected override string SerializeObject(Person person)
+        {
+            string message = $"<{nameof(Person)}>";
+
+            message += $"<{nameof(person.FullName)}>";
+            message += $"{person.FullName}";
+            message += $"</{nameof(person.FullName)}>";
+
+            message += $"<{nameof(person.Age)}>";
+            message += $"{person.Age}";
+            message += $"</{nameof(person.Age)}>";
+
+            message += $"<{nameof(person.Salary)}>";
+            message += $"{person.Salary}";
+            message += $"</{nameof(person.Salary)}>";
+
+            message += $"</{nameof(Person)}>";
+
+            return message;
+        }
+    }
+}
